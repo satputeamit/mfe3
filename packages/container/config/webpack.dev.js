4c45567b968d121bb560503e16d6e288c4f33870
satputeamit <satpute.amitkumar@gmail.com>
2023-12-28 18:40:56 +0530

    historyApiFallback modified

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -9,7 +9,7 @@ const devConfig = {
     devServer: {
         port: 8080,
         historyApiFallback: {
-            index: "index.hmtl"
+            index: "/index.html"
         }
     },
     plugins: [
